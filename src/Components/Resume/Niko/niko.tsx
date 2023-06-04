2667338312f35bbdae42f59baeb20d18d4ea3116
prajwal <112236602+Prajwal-shindee@users.noreply.github.com>
2023-06-04 13:55:40 +0530

    Update niko.tsx

--- src/Components/Resume/Niko/niko.tsx
+++ src/Components/Resume/Niko/niko.tsx
@@ -23,8 +23,8 @@ export const Niko = ({edit}:{edit:boolean})=>{
   
                   <div className="info">
                     <h5 className="title" ><small>WEB:</small></h5>
-                    <p className='c-link'  data-editable={edit} contentEditable={edit} >www.timi.dev#about-me</p>
-                    <p className='c-link'  data-editable={edit} contentEditable={edit} >www.github.com/Timi-Leyin</p>
+                    <p className='c-link'  data-editable={edit} contentEditable={edit} >https://prajwal-shinde.netlify.app/#about-me</p>
+                    <p className='c-link'  data-editable={edit} contentEditable={edit} >www.github.com/Prajwal-shindee</p>
                   </div>
                 </div>
                 <div className="con">
@@ -58,7 +58,7 @@ export const Niko = ({edit}:{edit:boolean})=>{
         <div className="about-intro">
         <div className="">
         <h1>
-            <p  data-editable={edit} contentEditable={edit}>TIMI LEYIN</p>
+            <p  data-editable={edit} contentEditable={edit}>Prajwal Shinde</p>
             <p  data-editable={edit} contentEditable={edit}>WEB DESIGNER</p>
           </h1>
         </div>
